14f07324cec93bcd8cef57bf3ea80f89519f2cde
James Daugherty <jdaugherty@jdresources.net>
2025-03-13 07:57:34 -0400

    Rework actions to eliminate the need for custom actions in grails-core

--- pre-release/entrypoint.sh
+++ pre-release/entrypoint.sh
@@ -1,56 +1,100 @@
 #!/bin/bash
-# $1 == GH_TOKEN
 
-if [ -z "$RELEASE_VERSION" ]; then
-    echo -n "Determining release version: "
-    release_version=${GITHUB_REF:11}
+# The following permissions are required to successfully execute this script:
+# permissions:
+#   contents: write // tags / commits
+
+set_value_or_error() {
+  local value="$1"
+  local defaultValue="$2"
+  local variableName="$3"
+  local validValues="${4:-}"  # optional argument (if empty, skip validation)
+
+  if [[ -z "$value" && -z "$defaultValue" ]]; then
+    echo "ERROR: A value for $variableName is required." >&2
+    exit 1
+  fi
+
+  if [[ -n "$value" ]]; then
+    decidedValue="$value"
+  else
+    echo "${variableName}: Using default value: ${defaultValue}"
+    decidedValue="$defaultValue"
+  fi
+
+  if [[ -n "$validValues" ]]; then
+    local match=false
+    local v
+    for v in $validValues; do
+      if [[ "$decidedValue" == "$v" ]]; then
+        match=true
+        break
+      fi
+    done
+
+    if ! $match; then
+      echo "ERROR: Invalid value for '$variableName': '$decidedValue'." >&2
+      echo "       Must be one of: $validValues" >&2
+      return 1
+    fi
+  fi
+
+  eval "$variableName=\"\$decidedValue\""
+}
+
+set -e
+
+set_value_or_error "${RELEASE_VERSION}" "${GITHUB_REF:11}" "RELEASE_VERSION"
+if [[ ! "$VERSION" =~ ^v?[^.]+\.[^.]+\.[^.]+$ ]]; then
+  echo "ERROR: VERSION must be in the format 'X.X.X' or 'vX.X.X'. Got: '$VERSION'"
+  exit 1
+fi
+if [[ "${RELEASE_VERSION}" == v* ]]; then
+  RELEASE_VERSION="${RELEASE_VERSION#v}"
 else
-    release_version=${RELEASE_VERSION}
+  RELEASE_VERSION="${RELEASE_VERSION}"
 fi
-echo $release_version
+echo "Release Version: ${RELEASE_VERSION}"
 
-if [ -n "$MICRONAUT_BUILD_EMAIL" ]; then
-    GIT_USER_EMAIL=$MICRONAUT_BUILD_EMAIL
-fi
+set_value_or_error "${RELEASE_URL}" `cat $GITHUB_EVENT_PATH | jq '.release.url' | sed -e 's/^"\(.*\)"$/\1/g'` "RELEASE_URL"
 
-if [ -z "$GIT_USER_NAME" ]; then
-   GIT_USER_NAME="micronaut-build"
-fi
+set_value_or_error "${GIT_USER_NAME}" "${GITHUB_ACTOR}" "GIT_USER_NAME"
+
+set_value_or_error "${GITHUB_WORKSPACE}" "." "GIT_SAFE_DIR"
 
 echo "Configuring git"
-git config --global --add safe.directory /github/workspace
-git config --global user.email "$GIT_USER_EMAIL"
-git config --global user.name "$GIT_USER_NAME"
+git config --global --add safe.directory "${GIT_SAFE_DIR}"
+git config --global user.email "${GITHUB_USER_NAME}@users.noreply.github.com"
+git config --global user.name "${GITHUB_USER_NAME}"
 git fetch
 
-if [ -z "$TARGET_BRANCH" ]; then
-    echo -n "Determining target branch: "
-    target_branch=`cat $GITHUB_EVENT_PATH | jq '.release.target_commitish' | sed -e 's/^"\(.*\)"$/\1/g'`
-    echo $target_branch
-else 
-    target_branch=$TARGET_BRANCH
-fi
-
-git checkout $target_branch
+echo -n "Determining target branch: "
+set_value_or_error "${TARGET_BRANCH}" `cat $GITHUB_EVENT_PATH | jq '.release.target_commitish' | sed -e 's/^"\(.*\)"$/\1/g'` "${TARGET_BRANCH}"
+echo "${TARGET_BRANCH}"
+git checkout "${TARGET_BRANCH}"
 
 echo "Setting release version in gradle.properties"
-sed -i "s/^projectVersion.*$/projectVersion\=${release_version}/" gradle.properties
+sed -i "s/^projectVersion.*$/projectVersion\=${RELEASE_VERSION}/" gradle.properties
 cat gradle.properties
-
-echo "Pushing release version and recreating v${release_version} tag"
 git add gradle.properties
-git commit -m "[skip ci] Release v${release_version}"
-git push origin $target_branch
-git tag -fa v${release_version} -m "Release v${release_version}"
-git push origin $target_branch
-# force push the updated tag
-git push origin v${release_version} --force
 
-echo "Closing again the release after updating the tag"
-if [ -z "$RELEASE_URL" ]; then
-    release_url=`cat $GITHUB_EVENT_PATH | jq '.release.url' | sed -e 's/^"\(.*\)"$/\1/g'`
+if [[ -n "${RELEASE_SCRIPT_PATH}" && -x "${RELEASE_SCRIPT_PATH}" ]]; then
+  echo "Executing additional release script at ${RELEASE_SCRIPT_PATH}"
+  "$RELEASE_SCRIPT_PATH"
 else
-    release_url=$RELEASE_URL
+  if [[ -n "${RELEASE_SCRIPT_PATH}" ]]; then
+    echo "ERROR: RELEASE_SCRIPT_PATH is set to '$THIRD_PARTY_SCRIPT_PATH' but is not executable or does not exist." >&2
+    exit 1
+  fi
 fi
-echo $release_url
-curl -s --request PATCH -H "Authorization: Bearer $1" -H "Content-Type: application/json" $release_url --data "{\"draft\": false}"
+
+echo "Pushing release version and recreating v${RELEASE_VERSION} tag"
+git commit -m "[skip ci] Release v${RELEASE_VERSION}"
+git push origin "${TARGET_BRANCH}"
+git tag -fa v${RELEASE_VERSION} -m "Release v${RELEASE_VERSION}"
+git push origin "${TARGET_BRANCH}"
+# force push the updated tag
+git push origin "v${RELEASE_VERSION}" --force
+
+echo "Closing the release after updating the tag: ${RELEASE_URL}"
+curl -s --request PATCH -H "Authorization: Bearer $1" -H "Content-Type: application/json" "${RELEASE_URL}" --data "{\"draft\": false}"
--- stop-oracle-cloud-runners/README.md
+++
@@ -1,3 +0,0 @@
-# Stop on-demand GitHub Action Runner hosted on Oracle Cloud Infrastructure (OCI)
-
-Stops the OCI runners.
